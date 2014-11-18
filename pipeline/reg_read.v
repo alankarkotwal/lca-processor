68c60713c177d7fa1e88ff6bbfeb6fedde61ee8d
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-11-19 00:28:15 +0530

    complete reg-read

--- pipeline/reg_read.v
+++ pipeline/reg_read.v
@@ -1,7 +1,19 @@
 `ifndef _REG_READ
 `define _REG_READ
 
-module reg_read();
+`include "../reg-file/register_file.v"
+`include "../misc/equal.v"
+
+module reg_read(readAdd1, readAdd2, regValue1, regValue2, equalValue, write, writeAdd, writeR7, inR7, clk, reset);
+
+	output [15:0] regValue1, regValue2;
+	output 	      equalValue;
+	input  [15:0] in, inR7;
+	input  [2:0]  readAdd1, readAdd2, writeAdd;
+	input	      write, writeR7, clk, reset;
+	
+	register_file rfile(.clk(clk), .out1(regValue1), .out2(regValue2), .readAdd1(readAdd1), .readAdd2(readAdd2), .write(write), .writeAdd(writeAdd), .writeR7(writeR7), .inR7(inR7), .in(in), .reset(reset));
+	equal eqCheck(.in1(regValue1), .in2(regValue2), .out(equalValue));
 
 endmodule
 
--- reg-file/register_file.v
+++ reg-file/register_file.v
@@ -1,17 +1,21 @@
 `ifndef _REGFILE
 `define _REGFILE
 
-module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, reset); // Modify to include R7 effects
+`include "../misc/demux.v"
+`include "../misc/mux.v"
+`include "../reg-file/register.v"
+
+module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, writeR7, inR7, in, reset); // Modify to include R7 effects
 
 	output [15:0] out1, out2;
-	input  [15:0] in;
+	input  [15:0] in, inR7;
 	input  [2:0]  readAdd1, readAdd2, writeAdd;
-	input         write, clk, reset;
+	input         write, clk, reset, writeR7;
 	
 	wire   [15:0] data0,  data1,  data2,  data3,  data4,  data5,  data6,  data7;
 	wire   [7:0]  writeLinesInit, writeLines;
 	
-	demux8 dem(writeAdd, writeLinesInit);
+	decode8 dem(writeAdd, writeLinesInit);
 	mux16x8 mux1(data0, data1, data2, data3, data4, data5, data6, data7, readAdd1, out1);
 	mux16x8 mux2(data0, data1, data2, data3, data4, data5, data6, data7, readAdd2, out2);
 	
@@ -22,7 +26,6 @@ module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, r
 	or a4(writeLines[4], write, ~writeLinesInit[4]);
 	or a5(writeLines[5], write, ~writeLinesInit[5]);
 	or a6(writeLines[6], write, ~writeLinesInit[6]);
-	or a7(writeLines[7], write, ~writeLinesInit[7]);
 	
 	register16 r0(clk, data0, in, writeLines[0], reset);
 	register16 r1(clk, data1, in, writeLines[1], reset);
@@ -31,7 +34,7 @@ module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, r
 	register16 r4(clk, data4, in, writeLines[4], reset);
 	register16 r5(clk, data5, in, writeLines[5], reset);
 	register16 r6(clk, data6, in, writeLines[6], reset);
-	register16 r7(clk, data7, in, writeLines[7], reset);
+	register16 r7(clk, data7, (writeR7==1'b1)?inR7:in, ~(writeLines[7] & (~write + ~writeR7)), reset);
 	
 endmodule
 
