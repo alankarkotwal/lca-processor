cf8c6b48a4ac8c4ea90897a1ce99ac5ff892141f
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-11-21 05:55:23 +0530

    modified mem_access

--- pipeline/mem_access.v
+++ pipeline/mem_access.v
@@ -5,16 +5,17 @@
 `include "../misc/plus_one.v"
 `include "../memory/data_mem.v"
 
-module mem_access(RAFromPipe, ALUOut, RASelectInput, WASelectInput, MemData, DataIn, WriteMem, RAFromPipeInc);
+module mem_access(RAFromPipe, ALUOut, RASelectInput, WASelectInput, MemData, DataIn, DataInSelect, WriteMem, RAFromPipeInc, SignalC);
 
 	output [15:0] MemData, RAFromPipeInc;
-	input  [15:0] ALUOut, RAFromPipe, DataIn;
-	input         RASelectInput, WASelectInput, WriteMem;
-	wire   [15:0] readAddSelected, writeAddSelected;
+	input  [15:0] ALUOut, RAFromPipe, DataIn, SignalC;
+	input         RASelectInput, WASelectInput, WriteMem, DataInSelect;
+	wire   [15:0] readAddSelected, writeAddSelected, DataInSelected;
 	
 	mux16x2 RASelect(.data0(RAFromPipe), .data1(ALUOut), .selectInput(RASelectInput), .out(readAddSelected));
 	mux16x2 WASelect(.data0(RAFromPipe), .data1(ALUOut), .selectInput(WASelectInput), .out(writeAddSelected));
-	data_mem DataMemory(.readAdd(readAddSelected), .out(MemData), .writeAdd(writeAddSelected), .in(DataIn), .write(WriteMem));
+	mux16x2 RASelect(.data0(DataIn), .data1(SignalC), .selectInput(DataInSelect), .out(DataInSelected));
+	data_mem DataMemory(.readAdd(readAddSelected), .out(MemData), .writeAdd(writeAddSelected), .in(DataInSelected), .write(WriteMem));
 	plus_one Inc(.in(RAFromPipe), .out(RAFromPipeInc));
 
 endmodule
